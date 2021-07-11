f1eb5b164caf984d16ecdd31bc1cdaffd5144595
ForteEscpae <sehun5515@naver.com>
2021-07-11 18:09:32 +0900

    add ester egg

--- cogs/FunnyCommandHandler.py
+++ cogs/FunnyCommandHandler.py
@@ -9,25 +9,25 @@ class FunnyCommandHandler(commands.Cog):
     @commands.command()
     async def 곤란(self, ctx):
         embed = discord.Embed()
-        embed.set_image(url="https://cdn.discordapp.com/attachments/685396623483994185/862703916822429746/calander.jpg")
+        embed.set_image(url="https://cdn.discordapp.com/attachments/863529285553618947/863708128659111956/KakaoTalk_20210711_072534641_01.jpg")
         await ctx.send(embed=embed)
 
     @commands.command()
     async def 분노(self, ctx):
         embed = discord.Embed()
-        embed.set_image(url="https://cdn.discordapp.com/attachments/685396623483994185/862703916822429746/calander.jpg")
+        embed.set_image(url="https://cdn.discordapp.com/attachments/863529285553618947/863708126883610654/KakaoTalk_20210711_072534641.jpg")
         await ctx.send(embed=embed)
 
     @commands.command()
     async def 격분(self, ctx):
         embed = discord.Embed()
-        embed.set_image(url="https://cdn.discordapp.com/attachments/685396623483994185/862703916822429746/calander.jpg")
+        embed.set_image(url="https://cdn.discordapp.com/attachments/863529285553618947/863708124901015572/KakaoTalk_20210711_072534641_03.jpg")
         await ctx.send(embed=embed)
 
     @commands.command()
     async def 칭찬(self, ctx):
         embed = discord.Embed()
-        embed.set_image(url="https://cdn.discordapp.com/attachments/685396623483994185/862703916822429746/calander.jpg")
+        embed.set_image(url="https://cdn.discordapp.com/attachments/863529285553618947/863708118654255114/KakaoTalk_20210711_072534641_02.jpg")
         await ctx.send(embed=embed)
 
 def setup(bot):
