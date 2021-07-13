7dbef9687428269b97028dc978d3925a07279c33
ForteEscpae <sehun5515@naver.com>
2021-07-14 04:54:44 +0900

    add additional function

--- cogs/FunnyCommandHandler.py
+++ cogs/FunnyCommandHandler.py
@@ -30,5 +30,39 @@ class FunnyCommandHandler(commands.Cog):
         embed.set_image(url="https://cdn.discordapp.com/attachments/863529285553618947/863708118654255114/KakaoTalk_20210711_072534641_02.jpg")
         await ctx.send(embed=embed)
 
+    @commands.command()
+    async def 웃프네요(self, ctx):
+        embed = discord.Embed()
+        embed.set_image(url="https://cdn.discordapp.com/attachments/863529285553618947/864593966389657660/ff5204660b3192f7.png")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def 웃음(self, ctx):
+        embed = discord.Embed()
+        embed.set_image(url="https://cdn.discordapp.com/attachments/863529285553618947/864593973976760320/c5efe7d8b57fa748.png")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def 인사(self, ctx):
+        embed = discord.Embed()
+        embed.set_image(
+            url="https://cdn.discordapp.com/attachments/863529285553618947/864593976366465084/a3fb08af21b05254.png")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def 슬픔(self, ctx):
+        embed = discord.Embed()
+        embed.set_image(
+            url="https://cdn.discordapp.com/attachments/863529285553618947/864593978196099092/ca70b83f7e1269a4.png")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def 참담(self, ctx):
+        embed = discord.Embed()
+        embed.set_image(
+            url="https://cdn.discordapp.com/attachments/863529285553618947/864594482380668928/afff2f1c589e0b0c.png")
+        await ctx.send(embed=embed)
+
+
 def setup(bot):
     bot.add_cog(FunnyCommandHandler(bot))
