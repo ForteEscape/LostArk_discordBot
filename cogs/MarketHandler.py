5dc32cda8e64d47a3dc676122c4d3f9cae757f5a
ForteEscape <sehun5515@naver.com>
2022-02-13 03:42:38 +0900

    try

--- cogs/MarketHandler.py
+++ cogs/MarketHandler.py
@@ -21,6 +21,7 @@ class MarketHandler(commands.Cog):
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-gpu")
         await ctx.send("working")
         driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
         await ctx.send("working - driver")
