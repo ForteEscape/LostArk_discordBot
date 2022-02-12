33dd8492a06a84a7e139857eff9fbbce9850ab38
ForteEscape <sehun5515@naver.com>
2022-02-13 02:28:16 +0900

    try 5

--- cogs/MarketHandler.py
+++ cogs/MarketHandler.py
@@ -16,12 +16,12 @@ class MarketHandler(commands.Cog):
 
     @commands.command()
     async def 시세요약(self, ctx):
-        chrome_options = webdriver.FirefoxOptions()
-        chrome_options.binary_location = os.environ.get("FIREFOX_BIN")
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
-        driver = webdriver.Firefox(executable_path=os.environ.get("GECKODRIVER_PATH"), options=chrome_options)
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
         #driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), options=chrome_options)
         #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
