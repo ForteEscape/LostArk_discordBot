118103ca8080c4badc13fa4c1a1098d218acecb4
ForteEscape <sehun5515@naver.com>
2022-02-13 01:42:35 +0900

    try1

--- cogs/MarketHandler.py
+++ cogs/MarketHandler.py
@@ -18,9 +18,9 @@ class MarketHandler(commands.Cog):
     async def 시세요약(self, ctx):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--no-sandbox')
+        #chrome_options.add_argument('--headless')
+        #chrome_options.add_argument('--disable-gpu')
+        #chrome_options.add_argument('--no-sandbox')
 
         driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), options=chrome_options)
         #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
