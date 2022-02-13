4e1bae26aa8730aba0af7afae010ace38b8ce3bf
ForteEscape <sehun5515@naver.com>
2022-02-13 16:07:05 +0900

    try

--- cogs/MarketHandler.py
+++ cogs/MarketHandler.py
@@ -41,8 +41,82 @@ class MarketHandler(commands.Cog):
             driver.get('http://lostark.game.onstove.com/Market')
             driver.implicitly_wait(10)
 
+            driver.find_element(By.XPATH, '//*[@id="user_id"]').send_keys('sehun8631@naver.com')
+            driver.implicitly_wait(10)
+            sleep(2)
+            driver.find_element(By.XPATH, '//*[@id="user_pwd"]').send_keys('kk2924140**')
+            driver.implicitly_wait(10)
+            sleep(2)
+            driver.find_element(By.XPATH, '//*[@id="idLogin"]/div[4]/button/span').click()
+            driver.implicitly_wait(10)
+            sleep(2)
+
             page = driver.page_source
-            await ctx.send(page[0:500])
+            await ctx.send(page + " loading success")
+
+            driver.find_element(By.XPATH, '//*[@id="lostark-wrapper"]/div/main/div/div[2]/a[2]').click()
+            driver.implicitly_wait(10)
+            price_data = driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]').text
+            driver.implicitly_wait(10)
+            sleep(2)
+
+            driver.find_element(By.XPATH, '//*[@id="marketList"]/div[2]/a[3]').click()
+            driver.implicitly_wait(10)
+            sleep(1)
+            price_data2 = driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]').text
+
+            page = driver.page_source
+            await ctx.send(page + " loading success")
+
+            data_price = price_data + '\n' + price_data2
+            data_price_list = data_price.split('\n')
+            data_list = []
+
+            name_list = [
+                ['파결석  ', '파괴석 결정'],
+                ['중레하  ', '중급 오레하 융화 재료'],
+                ['하레하  ', '하급 오레하 융화 재료'],
+                ['명  돌  ', '명예의 돌파석'],
+                ['위명돌  ', '위대한 명예의 돌파석'],
+                ['상레하  ', '상급 오레하 융화 재료'],
+                ['경명돌  ', '경이로운 명예의 돌파석'],
+                ['은  총  ', '태양의 은총'],
+                ['파강석  ', '파괴강석'],
+                ['축  복  ', '태양의 축복'],
+                ['명파(소)', '명예의 파편 주머니(소)'],
+                ['가  호  ', '태양의 가호'],
+                ['명파(중)', '명예의 파편 주머니(중)'],
+                ['명파(대)', '명예의 파편 주머니(대)'],
+            ]
+
+            for index in data_price_list:
+                if index == '시세 확인 구매' or index == '[10개 단위 판매]':
+                    pass
+                else:
+                    for name in name_list:
+                        if index in name:
+                            index = name[0]
+
+                    data_list.append(index)
+
+            temp_list = []
+            data_2d_list = []
+
+            for index in data_list:
+                temp_list.append(index)
+
+                if len(temp_list) == 4:
+                    data_2d_list.append(temp_list)
+                    temp_list = []
+
+            output = PrettyTable()
+
+            output.field_names = ["이름", "전날가", "최근가", "최저가"]
+
+            for index in data_2d_list:
+                output.add_row(index)
+
+            await ctx.send(f"```\n{output}\n```")
 
         except Exception as error:
             await ctx.send(error)
