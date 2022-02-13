536a0452f96a39bfb17b3907db88c8ca7e304505
ForteEscape <sehun5515@naver.com>
2022-02-13 14:35:03 +0900

    add

--- cogs/MarketHandler.py
+++ cogs/MarketHandler.py
@@ -17,7 +17,7 @@ class MarketHandler(commands.Cog):
     @commands.command()
     async def 시세요약(self, ctx):
         chrome_options = webdriver.ChromeOptions()
-        #chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+        chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
