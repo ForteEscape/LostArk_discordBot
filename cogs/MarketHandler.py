60751a5e912740ba2ae40929daf03cb12a5eeac2
ForteEscape <sehun5515@naver.com>
2022-02-14 14:55:16 +0900

    redesign market.py structure

--- cogs/MarketHandler.py
+++ cogs/MarketHandler.py
@@ -3,6 +3,7 @@ import os
 import discord
 from discord.ext import commands
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
@@ -17,38 +18,77 @@ class MarketHandler(commands.Cog):
 
     @commands.command()
     async def 시세요약(self, ctx):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--window-size=1920, 1080")
-        chrome_options.add_argument("--remote-debugging-port=9222")
-
-        driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
+        driver = self.__get_driver()
 
         driver.get('https://lostark.game.onstove.com/Market/BookMark')
 
-        driver.find_element(By.XPATH, '//*[@id="user_id"]').send_keys('id')
-        sleep(2)
-        driver.find_element(By.XPATH, '//*[@id="user_pwd"]').send_keys('pwd')
-        sleep(2)
-        driver.find_element(By.XPATH, '//*[@id="idLogin"]/div[4]/button/span').click()
+        self.__log_in_process(driver)
         sleep(2)
 
         price_data = driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]').text
         sleep(1)
-
         driver.find_element(By.XPATH, '//*[@id="marketList"]/div[2]/a[3]').click()
-        sleep(1)
+
+        sleep(2)
         price_data2 = driver.find_element(By.XPATH, '//*[@id="tbodyItemList"]').text
 
         data_price = price_data + '\n' + price_data2
         data_price_list = data_price.split('\n')
-        data_list = []
 
         driver.quit()
 
+        data_2d_list = self.__data_processing(data_price_list)
+        output = PrettyTable()
+
+        output.field_names = ["이름", "전날가", "최근가", "최저가"]
+
+        for index in data_2d_list:
+            output.add_row(index)
+
+        await ctx.send(f"```\n{output}\n```")
+
+    @commands.command()
+    async def 시세(self, ctx, *item_name):
+        input_data = ' '.join(item_name)
+
+        if item_name is None:
+            embed = discord.Embed(title="!시세 명령어 설명",
+                                  description="!시세 명령어에 대하여 설명합니다.")
+            embed.add_field(name="!시세 [아이템 이름]",
+                            value="입력받은 아이템의 가격을 출력합니다\n")
+            await ctx.send(embed=embed)
+        else:
+            driver = self.__get_driver()
+            driver.get('https://lostark.game.onstove.com/Market')
+
+            self.__log_in_process(driver)
+            sleep(2)
+
+            name_input = driver.find_element(By.XPATH, '/html/body/div[2]/div/main/div/div[3]/div[2]/form/fieldset/div/div[1]/input')
+            name_input.send_keys(input_data)
+            sleep(2)
+            search_btn = driver.find_element(By.XPATH, '/html/body/div[2]/div/main/div/div[3]/div[2]/form/fieldset/div/div[4]/button[1]')
+            search_btn.click()
+
+            sleep(2)
+            driver.implicitly_wait(10)
+            price_data = driver.find_element(By.XPATH, '/html/body/div[2]/div/main/div/div[3]/div[2]/div/div/div[1]/table/tbody').text
+            sleep(2)
+            data_price_list = price_data.split('\n')
+
+            driver.quit()
+
+            data_2d_list = self.__data_processing(data_price_list)
+            output = PrettyTable()
+
+            output.field_names = ["이름", "전날가", "최근가", "최저가"]
+
+            for index in data_2d_list:
+                output.add_row(index)
+
+            await ctx.send(f"```\n{output}\n```")
+
+    def __data_processing(self, data_price_list):
         name_list = [
             ['파결석  ', '파괴석 결정'],
             ['중레하  ', '중급 오레하 융화 재료'],
@@ -66,8 +106,9 @@ class MarketHandler(commands.Cog):
             ['명파(대)', '명예의 파편 주머니(대)'],
         ]
 
+        data_list = []
         for index in data_price_list:
-            if index == '시세 확인 구매' or index == '[10개 단위 판매]':
+            if index == '시세 확인 구매' or index == '[10개 단위 판매]' or index == '[구매 시 거래 불가]':
                 pass
             else:
                 for name in name_list:
@@ -86,111 +127,33 @@ class MarketHandler(commands.Cog):
                 data_2d_list.append(temp_list)
                 temp_list = []
 
-        output = PrettyTable()
+        return data_2d_list
 
-        output.field_names = ["이름", "전날가", "최근가", "최저가"]
+    def __log_in_process(self, driver):
+        driver.find_element(By.XPATH, '//*[@id="user_id"]').send_keys('id')
+        sleep(2)
+        driver.find_element(By.XPATH, '//*[@id="user_pwd"]').send_keys('pwd')
+        sleep(2)
+        driver.find_element(By.XPATH, '//*[@id="idLogin"]/div[4]/button/span').click()
 
-        for index in data_2d_list:
-            output.add_row(index)
+    def __get_driver(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920, 1080")
+        chrome_options.add_argument("--remote-debugging-port=9222")
 
-        print(data_2d_list)
+        # live service
+        driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
 
-        await ctx.send(f"```\n{output}\n```")
+        # ====================== testing service ==================================
+        # service = Service(ChromeDriverManager().install())
+        # driver = webdriver.Chrome(service=service, chrome_options=chrome_options)
+        # ====================== testing service end ==============================
 
-    @commands.command()
-    async def 시세(self, ctx, *item_name):
-        input_data = ' '.join(item_name)
-
-        if item_name is None:
-            embed = discord.Embed(title="!시세 명령어 설명",
-                                  description="!시세 명령어에 대하여 설명합니다.")
-            embed.add_field(name="!시세 [아이템 이름]",
-                            value="입력받은 아이템의 가격을 출력합니다\n")
-            await ctx.send(embed=embed)
-        else:
-            chrome_options = webdriver.ChromeOptions()
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument("--no-sandbox")
-            chrome_options.add_argument("--disable-gpu")
-            chrome_options.add_argument("--window-size=1920, 1080")
-            chrome_options.add_argument("--remote-debugging-port=9222")
-
-            driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
-
-            driver.get('https://lostark.game.onstove.com/Market')
-
-            driver.find_element(By.XPATH, '//*[@id="user_id"]').send_keys('id')
-            sleep(2)
-            driver.find_element(By.XPATH, '//*[@id="user_pwd"]').send_keys('pwd')
-            sleep(2)
-            driver.find_element(By.XPATH, '//*[@id="idLogin"]/div[4]/button/span').click()
-            sleep(2)
-
-            name_input = driver.find_element(By.XPATH, '/html/body/div[2]/div/main/div/div[3]/div[2]/form/fieldset/div/div[1]/input')
-            name_input.send_keys(input_data)
-            sleep(2)
-            search_btn = driver.find_element(By.XPATH, '/html/body/div[2]/div/main/div/div[3]/div[2]/form/fieldset/div/div[4]/button[1]')
-            search_btn.click()
-
-            sleep(2)
-            driver.implicitly_wait(10)
-            price_data = driver.find_element(By.XPATH, '/html/body/div[2]/div/main/div/div[3]/div[2]/div/div/div[1]/table/tbody').text
-            sleep(2)
-            data_price_list = price_data.split('\n')
-            data_list = []
-
-            driver.quit()
-
-            name_list = [
-                ['파결석  ', '파괴석 결정'],
-                ['중레하  ', '중급 오레하 융화 재료'],
-                ['하레하  ', '하급 오레하 융화 재료'],
-                ['명  돌  ', '명예의 돌파석'],
-                ['위명돌  ', '위대한 명예의 돌파석'],
-                ['상레하  ', '상급 오레하 융화 재료'],
-                ['경명돌  ', '경이로운 명예의 돌파석'],
-                ['은  총  ', '태양의 은총'],
-                ['파강석  ', '파괴강석'],
-                ['축  복  ', '태양의 축복'],
-                ['명파(소)', '명예의 파편 주머니(소)'],
-                ['가  호  ', '태양의 가호'],
-                ['명파(중)', '명예의 파편 주머니(중)'],
-                ['명파(대)', '명예의 파편 주머니(대)'],
-            ]
-
-            for index in data_price_list:
-                if index == '시세 확인 구매' or index == '[10개 단위 판매]' or index == '[구매 시 거래 불가]':
-                    pass
-                else:
-                    for name in name_list:
-                        if index in name:
-                            index = name[0]
-
-                    data_list.append(index)
-
-            temp_list = []
-            data_2d_list = []
-
-            for index in data_list:
-                temp_list.append(index)
-
-                if len(temp_list) == 4:
-                    data_2d_list.append(temp_list)
-                    temp_list = []
-
-            output = PrettyTable()
-
-            output.field_names = ["이름", "전날가", "최근가", "최저가"]
-
-            for index in data_2d_list:
-                output.add_row(index)
-
-            print(data_2d_list)
-
-            await ctx.send(f"```\n{output}\n```")
-
-
+        return driver
 
 def setup(bot):
     bot.add_cog(MarketHandler(bot))
--- cogs/PlayerContentHandler.py
+++ cogs/PlayerContentHandler.py
@@ -34,32 +34,7 @@ class PlayerContentHandler(commands.Cog):
             if player in nicknameList:
                 player = nicknameList[0]
 
-        """
-        player_dic = {'유파민': 'https://loawa.com/char/%EC%9C%A0%ED%8C%8C%EB%AF%BC',
-                      '나비릴': 'https://loawa.com/char/%EC%95%84%EB%93%9D%EB%B0%94%EB%93%9D%EC%97%B0%EA%B5%AC%EC%86%8C',
-                      '릴리찐따': 'https://loawa.com/char/%EB%A6%B4%EB%A6%AC%EC%B0%90%EB%94%B0',
-                      '풀피': 'https://loawa.com/char/%ED%8D%BD%ED%94%BC',
-                      '누쿠누쿠': 'https://loawa.com/char/%EC%9C%97%EC%A7%91%EC%B8%B5%EA%B0%84%EC%86%8C%EC%9D%8C%EB%AF%BC%ED%8F%90%EB%85%80',
-                      '차차': 'https://loawa.com/char/%EC%8A%A8%EC%83%81%EC%9D%98%EB%B9%9B',
-                      '댕루': 'https://loawa.com/char/%EC%96%B4%ED%9D%A5%ED%8E%80%EC%B9%98%EB%8B%A4%EB%83%A5',
-                      '우깡': 'https://loawa.com/char/%EB%A7%9B%EC%9E%88%EB%8A%94%EC%83%88%EC%9A%B4%EB%A7%A4%EC%9A%B0%EA%B9%A1',
-                      '체맛건': 'https://loawa.com/char/%EC%B2%B4%EB%A6%AC%EB%A7%9B%EB%82%98%EB%8A%94%EA%B1%B4%EC%8A%AC',
-                      '귤남': 'https://loawa.com/char/%EA%B0%90%EA%B7%A4%EB%86%8D%EC%9E%A5%EC%83%81%EB%82%A8%EC%9E%90',
-                      '쌀떡': 'https://loawa.com/char/%EA%B7%B8%EB%A3%A8%ED%86%A4%EC%BD%94%EC%9D%B8%EC%83%B5',
-                      '따별': 'https://loawa.com/char/MaiR',
-                      '차비': 'https://loawa.com/char/%EC%AE%B8%EC%98%A4%EC%98%A5',
-                      '고희원': 'https://loawa.com/char/%EA%B3%A0%ED%9D%AC%EC%9B%90',
-                      '뭉클': 'https://loawa.com/char/%EC%95%84%EA%B8%B0%EB%8C%95%EC%9F%9D',
-                      '마마': 'https://loawa.com/char/%EB%A7%88%EB%A7%88%EB%A0%88%ED%9B%84',
-                      '야스치이': 'https://loawa.com/char/2nd%EC%BB%B4%ED%8C%A8%EB%8B%88%EC%96%B8',
-                      '워붕쿤': 'https://loawa.com/char/210301%EC%8B%9C%EC%9E%91',
-                      '티인': 'https://loawa.com/char/%EB%84%A4%EC%BD%94%EB%A7%88%EC%B8%A0%EB%A6%AC',
-                      '대주': 'https://loawa.com/char/%EB%8C%80%EC%A5%AC%EC%9D%B8',
-                      '나그없': 'https://loawa.com/char/%EC%9A%B0%ED%95%9C%EC%8B%A4%EC%9C%A0%ED%86%B5',
-                      }
-        """
-
-        if player == None:
+        if player is None:
             embed = discord.Embed(title="!캐릭터정보 명령어 설명",
                                   description="!캐릭터정보 명령어에 대하여 설명합니다.")
             embed.add_field(name="!캐릭터정보 [누쿠누쿠, 차차]",
